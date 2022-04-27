7ccbf09d0ab4dd11922c75f01923516eae008007
mitchell-liatrio <mitchellp@liatrio.com>
2022-04-27 10:37:11 -0500

    feat: handle array of orgs and readme updates

--- org-disco/language-metrics.js
+++ org-disco/language-metrics.js
@@ -1,8 +1,8 @@
 const { Octokit } = require("@octokit/rest");
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-const ORGANIZATION = 'liatrio'
-const PERCENTAGE_OF_CODE_THRESHOLD = 25;
+const ORGANIZATION_LIST = ['liatrio']
+const PERCENTAGE_OF_CODE_THRESHOLD = 10;
 
 
 (async () => {
@@ -10,42 +10,44 @@ const PERCENTAGE_OF_CODE_THRESHOLD = 25;
         data: { login },
     } = await octokit.rest.users.getAuthenticated()
 
-    const allRepos = await octokit
-        .paginate(octokit.rest.repos.listForOrg, {
-            org: 'liatrio',
-            type: 'any',
-            per_page: 100
-        })
-
-    const repoNames = allRepos
-        .filter((repo) => !repo.archived)
-        .map((repo) => repo.name)
-
     let languagesInOrg = {}
-    const getTotalCodeSizeInBytes = obj => Object.values(obj).reduce((accumulator, value) => accumulator + value, 0);
 
-    for (let repo of repoNames) {
-        const repoLanguages = await octokit.rest.repos.listLanguages({
-            owner: ORGANIZATION,
-            repo
-        })
+    for (let org of ORGANIZATION_LIST) {
+        const allReposInOrg = await octokit
+            .paginate(octokit.rest.repos.listForOrg, {
+                org: org,
+                type: 'any',
+                per_page: 100
+            })
 
-        sumBytes = getTotalCodeSizeInBytes(repoLanguages.data);
+        const repoNames = allReposInOrg
+            .filter((repo) => !repo.archived)
+            .map((repo) => repo.name)
 
-        for (let languageName in repoLanguages.data) {
-            const percent = Math.round((repoLanguages.data[languageName] / sumBytes) * 100)
+        const getTotalCodeSizeInBytes = obj => Object.values(obj).reduce((accumulator, value) => accumulator + value, 0);
 
-            if (languagesInOrg[languageName] && percent >= PERCENTAGE_OF_CODE_THRESHOLD) {
-                languagesInOrg[languageName].count++
-                languagesInOrg[languageName].repos.push(repo)
-            } else {
-                languagesInOrg[languageName] = {
-                    count: 1,
-                    repos: [repo]
+        for (let repo of repoNames) {
+            const repoLanguages = await octokit.rest.repos.listLanguages({
+                owner: org,
+                repo
+            })
+
+            sumBytes = getTotalCodeSizeInBytes(repoLanguages.data);
+
+            for (let languageName in repoLanguages.data) {
+                const percent = Math.round((repoLanguages.data[languageName] / sumBytes) * 100)
+
+                if (languagesInOrg[languageName] && percent >= PERCENTAGE_OF_CODE_THRESHOLD) {
+                    languagesInOrg[languageName].count++
+                    languagesInOrg[languageName].repos.push(repo)
+                } else {
+                    languagesInOrg[languageName] = {
+                        count: 1,
+                        repos: [repo]
+                    }
                 }
             }
         }
     }
     console.table(languagesInOrg)
-
 })()
