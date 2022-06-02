c5e4c1d3209bed88cccef5321e23fcc99b4186ea
mitchell-liatrio <mitchellp@liatrio.com>
2022-06-02 14:15:45 -0500

    chore: fixing linting errors

---
+++ scripts/list-repos-with-file-extensions.js
@@ -0,0 +1,50 @@
+const { listFileCountInRepository, searchByCriteria } = require("../util/github");
+
+module.exports = {
+    description: "list repos matching organization and extensions",
+    options: {
+        organization: {
+            alias: "o",
+            demandOption: true,
+            describe: "the GitHub organization to search",
+            type: "string",
+        },
+        extensions: {
+            alias: "e",
+            demandOption: true,
+            describe: "an array of extensions being searched for",
+            type: "array",
+        },
+        count: {
+            alias: "c",
+            default: false,
+            describe: "boolean flag to enable total count of files, will drastically increase execution time",
+            type: "bool",
+        },
+    },
+    action: async (octokit, argv) => {
+        const reposMatchingExtensions = {};
+        let count = 1;
+
+        const formattedExtensionsString = "+extension:" + argv.extensions.join("+extension:");
+
+        const queryResults = await searchByCriteria(octokit, `*+org:${argv.organization}${formattedExtensionsString}`);
+
+        for (const result of queryResults) {
+            const repoName = result.repository.name;
+
+            if (reposMatchingExtensions[repoName]) {
+                reposMatchingExtensions[repoName].occurrences++;
+            } else {
+                const repoFileCount = await listFileCountInRepository(octokit, argv.organization, repoName);
+
+                reposMatchingExtensions[repoName] = {
+                    occurrences: 1,
+                    total_file_count: repoFileCount,
+                };
+            }
+        }
+
+        console.table(reposMatchingExtensions);
+    },
+};
