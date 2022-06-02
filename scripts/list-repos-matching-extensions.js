fdeeb850b3526fa5b464ce6fc6c945d13e4c3876
mitchell-liatrio <mitchellp@liatrio.com>
2022-06-02 10:01:58 -0500

    fix: add flag to handle total count

--- scripts/list-repos-matching-extensions.js
+++ scripts/list-repos-matching-extensions.js
@@ -1,42 +1,55 @@
 const { searchByCriteria } = require("../util/github");
 
 module.exports = {
-    description: "list language usage within an organization",
+    description: "list repos matching organization and extensions",
     options: {
         organization: {
             alias: "o",
             demandOption: true,
-            describe: "the GitHub organization to search for language usage",
+            describe: "the GitHub organization to search",
             type: "string"
         },
         extensions: {
             alias: "e",
             demandOption: true,
-            describe: "a comma seperated list of extensions being searched for",
-            type: "string"
+            describe: "an array of extensions being searched for",
+            type: "array"
         },
+        count: {
+            alias: "c",
+            default: false,
+            describe: "boolean flag to enable total count of files, will drastically reduce execution time",
+            type: "bool"
+        }
     },
     action: async (octokit, argv) => {
         const reposMatchingExtensions = {};
+        let count = 1;
 
-        const extensionsArray = argv.extensions.split(",")
-        const formattedExtensionsString = "+extension:" + extensionsArray.join("+extension:")
+        const formattedExtensionsString = "+extension:" + argv.extensions.join("+extension:")
 
         const queryResults = await searchByCriteria(octokit, `*+org:${argv.organization}${formattedExtensionsString}`)
 
         for (const result of queryResults) {
-            const repo_full_name = result.repository.full_name
+            const repoFullName = result.repository.full_name
 
-            if (reposMatchingExtensions[repo_full_name]) {
-                reposMatchingExtensions[repo_full_name].occurrences++
+            if (reposMatchingExtensions[repoFullName]) {
+                reposMatchingExtensions[repoFullName].occurrences++
             } else {
-                const fileSearch = await octokit.rest.search.code({
-                    q: `*+repo:${repo_full_name}`
-                })
-
-                reposMatchingExtensions[repo_full_name] = {
-                    occurrences: 1,
-                    total_file_count: fileSearch.data.total_count
+                if (argv.count) {
+                    const fileSearch = await octokit.rest.search.code({
+                        q: `*+repo:${repoFullName}`
+                    })
+                    count++
+    
+                    reposMatchingExtensions[repoFullName] = {
+                        occurrences: 1,
+                        total_file_count: fileSearch.data.total_count
+                    }
+                } else {
+                    reposMatchingExtensions[repoFullName] = {
+                        occurrences: 1
+                    }
                 }
             }
         }
