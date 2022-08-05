5e0d25595b5e0e8137356f9f56ed29856b99bf07
Mitchell Phillips <100593043+mitchell-liatrio@users.noreply.github.com>
2022-08-04 19:17:37 -0500

    feat: enterprise language list (#11)
    * quick commmit
    
    * quick commmit
    
    * fix: gets full repo list
    
    * feat: add support for Enterprise level language use discovery
    
    * chore: eslint fixes
    
    * chore: eslint fixes
    
    * Update scripts/list-languages-in-enterprise.js
    
    Co-authored-by: Alex Ashley <alexashley@users.noreply.github.com>
    
    * Update README.md
    
    Co-authored-by: Alex Ashley <alexashley@users.noreply.github.com>
    
    * Update scripts/list-languages-in-enterprise.js
    
    Co-authored-by: Alex Ashley <alexashley@users.noreply.github.com>
    
    * chore: resolve camelCase variable refs
    
    * chore: remove eslint exemption
    
    Co-authored-by: Chris Warren <chrisw@liatrio.com>
    Co-authored-by: Alex Ashley <alexashley@users.noreply.github.com>

---
+++ scripts/list-languages-in-enterprise.js
@@ -0,0 +1,98 @@
+const { listAllRepositoriesInOrganization } = require("../util/github");
+
+const getTotalCodeSizeInBytes = (obj) => Object.values(obj).reduce((accumulator, value) => accumulator + value, 0);
+
+module.exports = {
+    description: "list language usage within an enterprise",
+    options: {
+        enterprise: {
+            alias: "e",
+            demandOption: true,
+            describe: "the GitHub enterprise to search for language usage",
+            type: "string",
+        },
+        percent: {
+            alias: "p",
+            default: 25,
+            describe: "what percentage of code is needed to qualify as a \"used\" language",
+            type: "number",
+        },
+    },
+    action: async (octokit, graphql, argv) => {
+        const orgAndRepo = [];
+        let hasNextPage = true;
+        // workaround to avoid code duplication on next line, allows null initial value which graphql requires
+        // eslint-disable-next-line unicorn/no-null
+        let endCursor = null;
+        const languagesInEnterprise = {};
+
+        while (hasNextPage) {
+            const enterpriseInfo = await graphql(`
+                query ListOrgs {
+                  viewer {
+                    login
+                  }
+                  enterprise(slug: "${argv.enterprise}") {
+                    id
+                    organizations(
+                        first: 100
+                        after: ${endCursor}
+                      ) {
+                      nodes {
+                        url
+                      }
+                      pageInfo {
+                        startCursor
+                        hasPreviousPage
+                        hasNextPage
+                        endCursor
+                      }
+                    }
+                    slug
+                    name
+                  }
+                }
+            `);
+
+            hasNextPage = enterpriseInfo.enterprise.organizations.pageInfo.hasNextPage;
+            endCursor = `"${enterpriseInfo.enterprise.organizations.pageInfo.endCursor}"`;
+
+            const allOrgsInEnterprise = enterpriseInfo.enterprise.organizations.nodes.map((org) => org.url.slice(org.url.lastIndexOf("/") + 1));
+
+            for (const org of allOrgsInEnterprise) {
+                const repositories = await listAllRepositoriesInOrganization(octokit, org);
+                for (const repo of repositories) {
+                    orgAndRepo.push({
+                        organization: org,
+                        repository: repo.name,
+                    });
+                }
+            }
+        }
+
+        for (const repository of orgAndRepo) {
+            const languages = await octokit.rest.repos.listLanguages({
+                owner: repository.organization,
+                repo: repository.repository,
+            });
+
+            const sumBytes = getTotalCodeSizeInBytes(languages.data);
+
+            for (const language in languages.data) {
+                const percent = Math.round((languages.data[language] / sumBytes) * 100);
+
+                if (languagesInEnterprise[language] && percent >= argv.percent) {
+                    languagesInEnterprise[language].count++;
+                    languagesInEnterprise[language].repos.push(`${repository.organization}/${repository.repository}`);
+                } else {
+                    languagesInEnterprise[language] = {
+                        count: 1,
+                        repos: [`${repository.organization}/${repository.repository}`],
+                    };
+                }
+            }
+        }
+
+        console.table(languagesInEnterprise);
+    },
+};
