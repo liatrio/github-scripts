0439a91deeaef7c879415eaa0d2436ac71328227
Ethan Jones <77471034+ethanejones@users.noreply.github.com>
2023-04-20 08:05:50 -0500

    feat: Create output file in JSON format for list-languages-in-organization script (#22)
    * feat: create output file in JSON format
    
    * fix: resolve linting issues

--- scripts/list-languages-in-organization.js
+++ scripts/list-languages-in-organization.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const { listAllRepositoriesInOrganization } = require("../util/github");
 
 const getTotalCodeSizeInBytes = (obj) => Object.values(obj).reduce((accumulator, value) => accumulator + value, 0);
@@ -11,6 +13,11 @@ module.exports = {
             describe: "the GitHub organization to search for language usage",
             type: "string",
         },
+        outputFilePath: {
+            alias: "f",
+            describe: "the path of the output file to write the results to",
+            type: "string",
+        },
         percent: {
             alias: "p",
             default: 25,
@@ -20,6 +27,20 @@ module.exports = {
     },
     action: async (octokit, _graphql, argv) => {
         const languagesInOrg = {};
+        const currentDirectory = process.cwd();
+        const date = new Date().toISOString();
+        const outputFilePath = argv.outputFilePath || path.join(currentDirectory, `languages-in-organization.${argv.organization}.${date}.json`);
+
+        // check if argv.outputFilePath is set, if so check if the parent directory exists as we will not create it
+        if (argv.outputFilePath) {
+            const parentDirectory = path.join(argv.outputFilePath, "..");
+            // if the parent directory does not exist, exit the script
+            if (!fs.existsSync(parentDirectory)) {
+                console.error(`The parent directory of the provided output file path does not exist: '${parentDirectory}'`);
+
+                return;
+            }
+        }
 
         const repositories = await listAllRepositoriesInOrganization(octokit, argv.organization);
 
@@ -46,6 +67,20 @@ module.exports = {
             }
         }
 
+        // Print results to console in table format
+        // Repo arrays will be truncated if they contain more than 3 items
+        // Due to that, we will also write the results to a file
         console.table(languagesInOrg);
+
+        // Write results to file
+        fs.writeFile(outputFilePath, JSON.stringify(languagesInOrg, undefined, 2), (err) => {
+            if (err) {
+                console.error(err);
+
+                return;
+            }
+
+            console.log(`Output file has been created: '${outputFilePath}'`);
+        });
     },
 };
